e42766b7951056402738e88a0fd09b79e3ed8d1c
Leo Zhang <balabagi2012@gmail.com>
2020-12-15 20:05:54 +0800

    feat(View):completed UI

--- src/App.vue
+++ src/App.vue
@@ -7,24 +7,256 @@
       <el-row type="flex">
         <el-col :span="8" class="demo-container">
           <div class="demo-message-container">
-            <div class="demo-message-header">
-              <div
-                class="demo-message-img style1"
-                :style="`background-image:url(${imgUrl})`"
-              ></div>
+            <!-- Header Start -->
+            <div class="demo-message-header" :class="'style' + style">
+              <template v-for="headerItem in menu.header.payload">
+                <div
+                  class="demo-message-badge"
+                  v-if="headerItem.type === 'Badge'"
+                  :key="headerItem.payload"
+                >
+                  {{ headerItem.payload }}
+                </div>
+                <div
+                  class="demo-message-img"
+                  :class="'style' + style"
+                  v-else-if="headerItem.type === 'Image'"
+                  :key="headerItem.payload"
+                  :style="`background-image:url(${headerItem.payload})`"
+                ></div>
+                <div
+                  class="demo-message-button"
+                  :class="'style' + style"
+                  v-else-if="headerItem.type === 'Button'"
+                  :key="headerItem.payload"
+                >
+                  {{ headerItem.payload }}
+                </div>
+                <div
+                  class="demo-description-box"
+                  v-else-if="headerItem.type === 'Description'"
+                  :key="headerItem.payload.key"
+                >
+                  <div class="demo-description-key">
+                    {{ headerItem.payload.key }}
+                  </div>
+                  <div class="demo-description-value">
+                    {{ headerItem.payload.value }}
+                  </div>
+                </div>
+              </template>
             </div>
-            <div class="demo-message-body"></div>
+            <!-- Header End -->
+            <!-- Body Start -->
+            <div class="demo-message-body" :class="'style' + style">
+              <template v-for="bodyItem in menu.body.payload">
+                <div
+                  class="demo-message-badge"
+                  v-if="bodyItem.type === 'Badge'"
+                  :key="bodyItem.payload"
+                >
+                  {{ bodyItem.payload }}
+                </div>
+                <div
+                  class="demo-text-label"
+                  v-else-if="bodyItem.type === 'Label'"
+                  :key="bodyItem.payload"
+                >
+                  {{ bodyItem.payload }}
+                </div>
+                <div
+                  class="demo-description-box"
+                  v-else-if="bodyItem.type === 'Description'"
+                  :key="bodyItem.payload.key"
+                >
+                  <div class="demo-description-key">
+                    {{ bodyItem.payload.key }}
+                  </div>
+                  <div
+                    class="demo-description-value"
+                    v-if="bodyItem.payload.value"
+                  >
+                    {{ bodyItem.payload.value }}
+                  </div>
+                </div>
+                <div
+                  class="demo-buttons-box"
+                  :class="bodyItem.style"
+                  v-else-if="bodyItem.type === 'Buttons'"
+                  :key="bodyItem.type"
+                >
+                  <div
+                    class="demo-buttons-button"
+                    v-for="button in bodyItem.payload"
+                    :key="button.payload"
+                  >
+                    {{ button.payload }}
+                  </div>
+                </div>
+                <div
+                  class="demo-message-img"
+                  v-else-if="bodyItem.type === 'Image'"
+                  :key="bodyItem.payload"
+                  :style="`background-image:url(${bodyItem.payload})`"
+                ></div>
+                <div
+                  class="demo-message-separator"
+                  v-else-if="bodyItem.type === 'Separator'"
+                  :key="bodyItem.payload"
+                ></div>
+                <div
+                  class="demo-text-p"
+                  v-else-if="bodyItem.type === 'P'"
+                  :key="bodyItem.payload"
+                >
+                  {{ bodyItem.payload }}
+                </div>
+              </template>
+              <!-- Footer Start -->
+              <div class="demo-message-footer">
+                <template v-for="footerItem in menu.footer.payload">
+                  <div
+                    class="demo-text-note"
+                    v-if="footerItem.type === 'Note'"
+                    :key="footerItem.payload"
+                  >
+                    {{ footerItem.payload }}
+                  </div>
+                  <div
+                    v-else-if="footerItem.type === 'Description'"
+                    :key="footerItem.type"
+                    style="display: flex; flex-direction: row; width: 100%"
+                  >
+                    <div class="demo-footer-key">
+                      {{ footerItem.payload.key }}
+                    </div>
+                    <div class="demo-footer-value">
+                      {{ footerItem.payload.value }}
+                    </div>
+                  </div>
+                </template>
+              </div>
+              <!-- Footer End -->
+            </div>
           </div>
         </el-col>
-        <el-col :span="8" class="node-container"> </el-col>
+        <el-col :span="8" class="node-container">
+          <el-row>
+            <el-button type="warning" @click="addImage">新增圖片</el-button>
+            <el-button type="primary" @click="addDescription"
+              >新增敘述</el-button
+            >
+            <el-button type="success" @click="addButton">新增按鈕</el-button>
+          </el-row>
+          <el-select
+            v-model="style"
+            placeholder="請選擇"
+            style="width: 100%; margin-top: 20px"
+          >
+            <el-option
+              v-for="n in 6"
+              :key="'樣式' + n"
+              :label="'樣式' + n"
+              :value="n"
+            >
+            </el-option>
+          </el-select>
+          <el-menu
+            default-active="1"
+            :collapse="isCollapse"
+            @select="onSelectMenuItem"
+            background-color="transparent"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+          >
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>header</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item
+                  :index="`header-${i}`"
+                  :key="`header-${i}`"
+                  v-for="(item, i) in menu.header.payload"
+                  >{{ item.type }}</el-menu-item
+                >
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="2">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>body</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item
+                  :index="`body-${i}`"
+                  :key="`body-${i}`"
+                  v-for="(item, i) in menu.body.payload"
+                  >{{ item.type }}</el-menu-item
+                >
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="3">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>footer</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item
+                  :index="`footer-${i}`"
+                  :key="`footer-${i}`"
+                  v-for="(item, i) in menu.footer.payload"
+                  >{{ item.type }}</el-menu-item
+                >
+              </el-menu-item-group>
+            </el-submenu>
+          </el-menu>
+        </el-col>
         <el-col :span="8" class="form-container">
           <el-form ref="form" :model="form" label-width="80px">
-            <el-form-item label="背景圖片"> </el-form-item>
-            <el-form-item label="Ratio"> </el-form-item>
-            <el-form-item label="顯示圖片"> </el-form-item>
-            <el-form-item label="可選區域"> </el-form-item>
-            <el-form-item label="按鈕文字">
-              <el-input type="text" v-model="text"></el-input>
+            <el-form-item :label="selectMenuItem.type"></el-form-item>
+            <el-form-item
+              label="顯示文字"
+              v-if="selectMenuItem.type === 'Badge'"
+            >
+              <el-input type="text" v-model="selectMenuItem.payload"></el-input>
+            </el-form-item>
+            <el-form-item
+              :label="'按鈕' + index + '文字'"
+              v-for="(button, index) in selectMenuItem.type === 'Buttons'
+                ? selectMenuItem.payload
+                : []"
+              :key="button.payload"
+            >
+              <el-input type="text" v-model="button.payload"></el-input>
+            </el-form-item>
+            <el-form-item
+              :label="'按鈕文字'"
+              v-if="selectMenuItem.type === 'Button'"
+            >
+              <el-input type="text" v-model="selectMenuItem.payload"></el-input>
+            </el-form-item>
+            <el-form-item
+              :label="'標題文字'"
+              v-if="selectMenuItem.type === 'Description'"
+            >
+              <el-input
+                type="text"
+                v-model="selectMenuItem.payload.key"
+              ></el-input>
+            </el-form-item>
+            <el-form-item
+              :label="'內容文字'"
+              v-if="
+                selectMenuItem.type === 'Description' &&
+                selectMenuItem.payload.value
+              "
+            >
+              <el-input
+                type="text"
+                v-model="selectMenuItem.payload.value"
+              ></el-input>
             </el-form-item>
           </el-form>
         </el-col>
@@ -47,17 +279,69 @@
 
 <script>
 import { nanoid } from "nanoid";
+import * as Mock from "@/mock";
+import _ from "lodash";
 export default {
   name: "App",
   components: {},
+  watch: {
+    style: {
+      handler(newValue) {
+        console.log(Mock[`style${newValue}`]);
+        this.menu = Mock[`style${newValue}`];
+      },
+      immediate: false,
+    },
+  },
   data() {
     return {
+      selectMenuIndex: "header-0",
+      menu: Mock.style1,
+      form: {},
+      style: 1,
+      ratio: 1,
+      isCollapse: false,
       showClipper: false,
       imgUrl:
         "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+      qrCodeUrl:
+        "https://scdn.line-apps.com/n/channel_devcenter/img/fx/linecorp_code_withborder.png",
     };
   },
-  methods: {},
+  methods: {
+    onSelectMenuItem(index) {
+      this.selectMenuIndex = index;
+    },
+    addImage() {
+      const newImage = {
+        type: "圖片",
+        label: "圖片-" + (this.images.length + 1),
+      };
+      this.menu.push(newImage);
+    },
+    addDescription() {
+      const newDescription = {
+        type: "描述",
+        label: "描述-" + (this.descriptions.length + 1),
+        key: "key",
+        value: "value",
+      };
+      this.menu.push(newDescription);
+    },
+    addButton() {
+      const newButton = {
+        type: "按鈕",
+        label: "按鈕-" + (this.buttons.length + 1),
+      };
+      this.menu.push(newButton);
+    },
+  },
+  computed: {
+    selectMenuItem() {
+      const [type, i] = this.selectMenuIndex.split("-");
+      return this.menu[type].payload[i];
+    },
+  },
 };
 </script>
 
@@ -99,29 +383,236 @@ export default {
     justify-content: center;
     align-items: center;
     border-radius: 17px;
+
     .demo-message-header {
       width: 100%;
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      &.style3 {
+        .demo-message-button {
+          display: none;
+        }
+      }
+      .demo-message-badge {
+        position: absolute;
+        background-color: #ff334b;
+        top: 20px;
+        left: 20px;
+        border-radius: 20px;
+        font-size: 13px;
+        color: #ffffff;
+        padding: 3px 5px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
       .demo-message-img {
         width: 100%;
+        background-origin: center;
+        background-position: center;
+        background-size: cover;
         border-top-left-radius: 17px;
         border-top-right-radius: 17px;
-        &.style1 {
+        &.style1,
+        &.style2 {
           height: 300px;
           border-bottom-left-radius: 17px;
           border-bottom-right-radius: 17px;
         }
-        &.style2 {
+        &.style3,
+        &.style4,
+        &.style5 {
           height: 195px;
+        }
+      }
+      .demo-message-button {
+        position: absolute;
+        bottom: 20px;
+        border-radius: 12px;
+        padding: 12px 16px;
+        background-color: rgb(220, 223, 229);
+        font-size: 20px;
+        color: rgb(68, 68, 68);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      .demo-description-box {
+        position: absolute;
+        bottom: 20px;
+        left: 20px;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        .demo-description-key {
+          font-size: 22px;
+          color: #fff;
+        }
+        .demo-description-value {
+          font-size: 16px;
+          margin-top: 3px;
+          color: #fff;
         }
       }
     }
     .demo-message-body {
       display: flex;
+      flex-direction: column;
       background-color: #fff;
       width: 100%;
+      padding: 20px;
+      box-sizing: border-box;
+      &.style1,
       &.style2 {
+        display: none;
+      }
+      &.style3,
+      &.style4,
+      &.style5 {
         border-bottom-left-radius: 17px;
         border-bottom-right-radius: 17px;
+      }
+      &.style6 {
+        border-radius: 17px;
+        .demo-description-box {
+          width: 100%;
+          height: 30px;
+          .demo-description-key {
+            color: #555;
+          }
+          .demo-description-value {
+            flex: 1;
+            text-align: right;
+            color: #111;
+          }
+        }
+        .demo-text-p {
+          color: #aaaaaa;
+        }
+      }
+      &.style4 {
+        background-color: #464f69;
+        .demo-text-label {
+          color: #fff;
+        }
+        .demo-description-box {
+          margin-top: -4px;
+          margin-bottom: 8px;
+          .demo-description-key {
+            color: #fff;
+          }
+          .demo-description-value {
+            color: #fff;
+          }
+        }
+        .demo-text-p {
+          color: rgb(255, 255, 255);
+        }
+      }
+      &.style5 {
+        .demo-description-box {
+          .demo-description-key {
+            width: 51px;
+          }
+        }
+      }
+      .demo-message-badge {
+        color: rgb(29, 180, 70);
+        margin-bottom: 8px;
+      }
+      .demo-message-separator {
+        border-bottom: 1px rgb(212, 214, 218) solid;
+        padding-top: 20px;
+        margin-bottom: 20px;
+        width: 100%;
+      }
+      .demo-text-label {
+        color: rgb(68, 68, 68);
+        font-weight: 700;
+        font-size: 22px;
+        margin-bottom: 12px;
+      }
+      .demo-buttons-box {
+        display: flex;
+        width: 100%;
+        .demo-buttons-button {
+          flex: 1;
+          font-size: 16px;
+          font-weight: 400;
+          display: flex;
+          flex-direction: row;
+          justify-content: center;
+          align-items: center;
+          text-align: center;
+          color: #42659a;
+        }
+        &.vertical {
+          flex-direction: column;
+          .demo-buttons-button {
+            padding: 8px 0;
+            box-sizing: content-box;
+          }
+        }
+        &.horizontal {
+          flex-direction: row;
+          justify-content: space-between;
+          margin-top: 20px;
+        }
+      }
+      .demo-description-box {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: center;
+        .demo-description-key {
+          font-size: 14px;
+          font-weight: 400;
+          line-height: 20px;
+          color: rgb(170, 170, 170);
+        }
+        .demo-description-value {
+          font-size: 14px;
+          flex: 1;
+          margin-left: 3px;
+          font-weight: 400;
+          line-height: 20px;
+          color: rgb(102, 102, 102);
+        }
+      }
+      .demo-message-img {
+        width: 140px;
+        height: 140px;
+        background-size: cover;
+        margin: 20px auto;
+      }
+    }
+    .demo-message-footer {
+      display: flex;
+      flex-direction: row;
+      width: 100%;
+      .demo-text-note {
+        font-size: 13px;
+        font-weight: normal;
+        color: rgb(170, 170, 170);
+      }
+      .demo-footer-key {
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 20px;
+        color: rgb(170, 170, 170);
+      }
+      .demo-footer-value {
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 20px;
+        color: rgb(170, 170, 170);
+        flex: 1;
+        text-align: right;
       }
     }
   }
---
+++ src/mock/index.js
@@ -0,0 +1,291 @@
+export const style1 = {
+  header: {
+    payload: [
+      {
+        type: "Image",
+        payload:
+          "https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip1.jpg",
+      },
+      {
+        type: "Badge",
+        payload: "SALE",
+      },
+      {
+        type: "Button",
+        payload: "點我查看",
+      },
+    ],
+  },
+  body: {
+    backgroundColor: "#fff",
+    payload: [],
+  },
+  footer: {
+    payload: [],
+  },
+};
+
+export const style2 = {
+  header: {
+    payload: [
+      {
+        type: "Image",
+        payload:
+          "https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip3.jpg",
+      },
+      {
+        type: "Description",
+        payload: {
+          key: "標題",
+          value: "段落一",
+        },
+      },
+    ],
+  },
+  body: {
+    backgroundColor: "#fff",
+    payload: [],
+  },
+  footer: {
+    payload: [],
+  },
+};
+
+export const style3 = {
+  header: {
+    payload: [
+      {
+        type: "Image",
+        payload:
+          "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+      },
+    ],
+  },
+  body: {
+    backgroundColor: "#fff",
+    payload: [
+      {
+        type: "Label",
+        payload: "標題",
+      },
+      {
+        type: "Description",
+        payload: {
+          key: "欄位一",
+          value: "段落一",
+        },
+      },
+      {
+        type: "Description",
+        payload: {
+          key: "欄位二",
+          value: "段落一",
+        },
+      },
+      {
+        type: "Buttons",
+        style: "horizontal",
+        payload: [
+          { type: "Button", payload: "按鈕一" },
+          { type: "Button", payload: "按鈕二" },
+          { type: "Button", payload: "按鈕三" },
+        ],
+      },
+    ],
+  },
+  footer: {
+    payload: [],
+  },
+};
+
+export const style4 = {
+  header: {
+    payload: [
+      {
+        type: "Image",
+        payload:
+          "https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip4.jpg",
+      },
+      {
+        type: "Badge",
+        payload: "NEW",
+      },
+    ],
+  },
+  body: {
+    backgroundColor: "#fff",
+    payload: [
+      {
+        type: "Label",
+        payload: "標題",
+      },
+      {
+        type: "Description",
+        payload: {
+          key: "欄位一",
+        },
+      },
+      {
+        type: "P",
+        payload:
+          "更多敘述更多敘述更多敘述更多敘更更多敘述多敘述更多敘述更多敘述更多敘述述更多敘述更多敘述更多敘述更多敘述",
+      },
+    ],
+  },
+  footer: {
+    payload: [],
+  },
+};
+
+export const style5 = {
+  header: {
+    payload: [
+      {
+        type: "Image",
+        payload:
+          "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_3_movie.png",
+      },
+    ],
+  },
+  body: {
+    backgroundColor: "#fff",
+    payload: [
+      {
+        type: "Label",
+        payload: "標題",
+      },
+      {
+        type: "Description",
+        payload: {
+          key: "欄位一",
+          value: "更多內容",
+        },
+      },
+      {
+        type: "Description",
+        payload: {
+          key: "欄位二",
+          value: "更多內容",
+        },
+      },
+      {
+        type: "Description",
+        payload: {
+          key: "欄位三",
+          value: "更多內容",
+        },
+      },
+      {
+        type: "Image",
+        payload:
+          "https://scdn.line-apps.com/n/channel_devcenter/img/fx/linecorp_code_withborder.png",
+      },
+    ],
+  },
+  footer: {
+    payload: [
+      {
+        type: "Note",
+        payload: "更多備註更多備註更多備註更多備註",
+      },
+    ],
+  },
+};
+
+export const style6 = {
+  header: {
+    payload: [],
+  },
+  body: {
+    backgroundColor: "#fff",
+    payload: [
+      {
+        type: "Badge",
+        payload: "Receipt",
+      },
+      {
+        type: "Label",
+        payload: "標題",
+      },
+      {
+        type: "P",
+        payload:
+          "更多敘述更多敘述更多敘述更多敘更更多敘述多敘述更多敘述更多敘述更多敘述述更多敘述更多敘述更多敘述更多敘述",
+      },
+      {
+        type: "Separator",
+        payload:"1",
+      },
+      {
+        type: "Description",
+        payload: {
+          key: "欄位一",
+          value: "更多內容",
+        },
+      },
+      {
+        type: "Description",
+        payload: {
+          key: "欄位二",
+          value: "更多內容",
+        },
+      },
+      {
+        type: "Description",
+        payload: {
+          key: "欄位三",
+          value: "更多內容",
+        },
+      },
+      {
+        type: "Separator",
+        payload: 
+          "2"
+        
+      },
+      {
+        type: "Description",
+        payload: {
+          key: "欄位4",
+          value: "更多內容",
+        },
+      },
+      {
+        type: "Description",
+        payload: {
+          key: "欄位5",
+          value: "更多內容",
+        },
+      },
+      {
+        type: "Description",
+        payload: {
+          key: "欄位6",
+          value: "更多內容",
+        },
+      },
+      {
+        type: "Description",
+        payload: {
+          key: "欄位7",
+          value: "更多內容",
+        },
+      },
+      {
+        type: "Separator",
+        payload:  "3",
+      },
+    ],
+  },
+  footer: {
+    payload: [
+      {
+        type: "Description",
+        payload: {
+          key: "欄位",
+          value: "更多內容",
+        },
+      },
+    ],
+  },
+};
